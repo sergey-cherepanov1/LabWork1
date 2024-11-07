841053cad7964a83240d76be7f189e1465ef780d
kaido <st129987@student.spbu.ru>
2024-11-07 21:05:55 +0300

    removed old funcs files

--- loadBMP.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOADBMP_h
-#define LOADBMP_h
-
-#include "bmp.h"
-
-bool loadBMP(const std::string& filename, std::vector<uint8_t>& imageData, int& width, int& height);
-
-#endif
--- rotate90.h
+++
@@ -1,9 +0,0 @@
-#ifndef ROTATE90_h
-#define ROTATE90_h
-
-#include <iostream>
-#include <vector>
-
-void rotate90(std::vector<uint8_t>& imageData, int& width, int& height);
-
-#endif
